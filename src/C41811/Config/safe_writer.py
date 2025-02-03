5610ccf86c8b04b5dc9d8febc12c0578684cbf35
C418____11 <553515788@qq.com>
2025-02-03 19:34:51 +0800

    LockFlags现在正确的继承自IntEnum 优化文档

--- src/C41811/Config/safe_writer.py
+++ src/C41811/Config/safe_writer.py
@@ -4,31 +4,32 @@
 
 # noinspection SpellCheckingInspection, GrazieInspection
 """
+  从 https://github.com/untitaker/python-atomicwrites 修改而来
+
+  .. code-block:: text
+    :caption: 原始版权声明
+
+    Copyright (c) 2015-2016 Markus Unterwaditzer
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+    of the Software, and to permit persons to whom the Software is furnished to do
+    so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
 .. versionadded:: 0.1.6
-
-从 https://github.com/untitaker/python-atomicwrites 修改而来
-
-原始代码的版权声明：
-
-Copyright (c) 2015-2016 Markus Unterwaditzer
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 """
 
 import os
@@ -39,7 +40,7 @@ from abc import ABC
 from abc import abstractmethod
 from contextlib import contextmanager
 from contextlib import suppress
-from enum import IntFlag
+from enum import IntEnum
 from numbers import Real
 from threading import Lock
 from typing import IO, ContextManager
@@ -282,7 +283,7 @@ class TempTextIOManager[F: TextIO](ABCTempIOManager):
             move_atomic(temp_file.name, path)
 
 
-class LockFlags(IntFlag):
+class LockFlags(IntEnum):
     """
     文件锁标志
     """
